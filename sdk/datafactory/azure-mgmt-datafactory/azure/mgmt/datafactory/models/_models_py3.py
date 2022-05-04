51b954cf27825ebb45e9f67a28a19d9be9c619b3
SDKAuto <sdkautomation@microsoft.com>
2022-05-04 19:53:36 +0000

    CodeGen from PR 18906 in Azure/azure-rest-api-specs Merge 74cc247d2d1dc6bf1697969c1bf26a135c2c3818 into 50ed15bd61ac79f2368d769df0c207a00b9e099f

--- sdk/datafactory/azure-mgmt-datafactory/azure/mgmt/datafactory/models/_models_py3.py
+++ sdk/datafactory/azure-mgmt-datafactory/azure/mgmt/datafactory/models/_models_py3.py
@@ -17986,6 +17986,8 @@ class DataFlowSink(Transformation):
     :vartype flowlet: ~azure.mgmt.datafactory.models.DataFlowReference
     :ivar schema_linked_service: Schema linked service reference.
     :vartype schema_linked_service: ~azure.mgmt.datafactory.models.LinkedServiceReference
+    :ivar rejected_data_linked_service: Rejected data linked service reference.
+    :vartype rejected_data_linked_service: ~azure.mgmt.datafactory.models.LinkedServiceReference
     """
 
     _validation = {
@@ -17999,6 +18001,7 @@ class DataFlowSink(Transformation):
         'linked_service': {'key': 'linkedService', 'type': 'LinkedServiceReference'},
         'flowlet': {'key': 'flowlet', 'type': 'DataFlowReference'},
         'schema_linked_service': {'key': 'schemaLinkedService', 'type': 'LinkedServiceReference'},
+        'rejected_data_linked_service': {'key': 'rejectedDataLinkedService', 'type': 'LinkedServiceReference'},
     }
 
     def __init__(
@@ -18010,6 +18013,7 @@ class DataFlowSink(Transformation):
         linked_service: Optional["LinkedServiceReference"] = None,
         flowlet: Optional["DataFlowReference"] = None,
         schema_linked_service: Optional["LinkedServiceReference"] = None,
+        rejected_data_linked_service: Optional["LinkedServiceReference"] = None,
         **kwargs
     ):
         """
@@ -18025,9 +18029,12 @@ class DataFlowSink(Transformation):
         :paramtype flowlet: ~azure.mgmt.datafactory.models.DataFlowReference
         :keyword schema_linked_service: Schema linked service reference.
         :paramtype schema_linked_service: ~azure.mgmt.datafactory.models.LinkedServiceReference
+        :keyword rejected_data_linked_service: Rejected data linked service reference.
+        :paramtype rejected_data_linked_service: ~azure.mgmt.datafactory.models.LinkedServiceReference
         """
         super(DataFlowSink, self).__init__(name=name, description=description, dataset=dataset, linked_service=linked_service, flowlet=flowlet, **kwargs)
         self.schema_linked_service = schema_linked_service
+        self.rejected_data_linked_service = rejected_data_linked_service
 
 
 class DataFlowSource(Transformation):
@@ -42452,6 +42459,8 @@ class PowerQuerySink(DataFlowSink):
     :vartype flowlet: ~azure.mgmt.datafactory.models.DataFlowReference
     :ivar schema_linked_service: Schema linked service reference.
     :vartype schema_linked_service: ~azure.mgmt.datafactory.models.LinkedServiceReference
+    :ivar rejected_data_linked_service: Rejected data linked service reference.
+    :vartype rejected_data_linked_service: ~azure.mgmt.datafactory.models.LinkedServiceReference
     :ivar script: sink script.
     :vartype script: str
     """
@@ -42467,6 +42476,7 @@ class PowerQuerySink(DataFlowSink):
         'linked_service': {'key': 'linkedService', 'type': 'LinkedServiceReference'},
         'flowlet': {'key': 'flowlet', 'type': 'DataFlowReference'},
         'schema_linked_service': {'key': 'schemaLinkedService', 'type': 'LinkedServiceReference'},
+        'rejected_data_linked_service': {'key': 'rejectedDataLinkedService', 'type': 'LinkedServiceReference'},
         'script': {'key': 'script', 'type': 'str'},
     }
 
@@ -42479,6 +42489,7 @@ class PowerQuerySink(DataFlowSink):
         linked_service: Optional["LinkedServiceReference"] = None,
         flowlet: Optional["DataFlowReference"] = None,
         schema_linked_service: Optional["LinkedServiceReference"] = None,
+        rejected_data_linked_service: Optional["LinkedServiceReference"] = None,
         script: Optional[str] = None,
         **kwargs
     ):
@@ -42495,10 +42506,12 @@ class PowerQuerySink(DataFlowSink):
         :paramtype flowlet: ~azure.mgmt.datafactory.models.DataFlowReference
         :keyword schema_linked_service: Schema linked service reference.
         :paramtype schema_linked_service: ~azure.mgmt.datafactory.models.LinkedServiceReference
+        :keyword rejected_data_linked_service: Rejected data linked service reference.
+        :paramtype rejected_data_linked_service: ~azure.mgmt.datafactory.models.LinkedServiceReference
         :keyword script: sink script.
         :paramtype script: str
         """
-        super(PowerQuerySink, self).__init__(name=name, description=description, dataset=dataset, linked_service=linked_service, flowlet=flowlet, schema_linked_service=schema_linked_service, **kwargs)
+        super(PowerQuerySink, self).__init__(name=name, description=description, dataset=dataset, linked_service=linked_service, flowlet=flowlet, schema_linked_service=schema_linked_service, rejected_data_linked_service=rejected_data_linked_service, **kwargs)
         self.script = script
 
 
@@ -42990,6 +43003,31 @@ class PrestoSource(TabularSource):
         self.query = query
 
 
+class PrivateEndpoint(msrest.serialization.Model):
+    """Private endpoint which a connection belongs to.
+
+    :ivar id: The resource Id for private endpoint.
+    :vartype id: str
+    """
+
+    _attribute_map = {
+        'id': {'key': 'id', 'type': 'str'},
+    }
+
+    def __init__(
+        self,
+        *,
+        id: Optional[str] = None,
+        **kwargs
+    ):
+        """
+        :keyword id: The resource Id for private endpoint.
+        :paramtype id: str
+        """
+        super(PrivateEndpoint, self).__init__(**kwargs)
+        self.id = id
+
+
 class PrivateEndpointConnectionListResponse(msrest.serialization.Model):
     """A list of linked service resources.
 
@@ -43080,25 +43118,32 @@ class PrivateLinkConnectionApprovalRequest(msrest.serialization.Model):
     :ivar private_link_service_connection_state: The state of a private link connection.
     :vartype private_link_service_connection_state:
      ~azure.mgmt.datafactory.models.PrivateLinkConnectionState
+    :ivar private_endpoint: The resource of private endpoint.
+    :vartype private_endpoint: ~azure.mgmt.datafactory.models.PrivateEndpoint
     """
 
     _attribute_map = {
         'private_link_service_connection_state': {'key': 'privateLinkServiceConnectionState', 'type': 'PrivateLinkConnectionState'},
+        'private_endpoint': {'key': 'privateEndpoint', 'type': 'PrivateEndpoint'},
     }
 
     def __init__(
         self,
         *,
         private_link_service_connection_state: Optional["PrivateLinkConnectionState"] = None,
+        private_endpoint: Optional["PrivateEndpoint"] = None,
         **kwargs
     ):
         """
         :keyword private_link_service_connection_state: The state of a private link connection.
         :paramtype private_link_service_connection_state:
          ~azure.mgmt.datafactory.models.PrivateLinkConnectionState
+        :keyword private_endpoint: The resource of private endpoint.
+        :paramtype private_endpoint: ~azure.mgmt.datafactory.models.PrivateEndpoint
         """
         super(PrivateLinkConnectionApprovalRequest, self).__init__(**kwargs)
         self.private_link_service_connection_state = private_link_service_connection_state
+        self.private_endpoint = private_endpoint
 
 
 class PrivateLinkConnectionApprovalRequestResource(SubResource):
