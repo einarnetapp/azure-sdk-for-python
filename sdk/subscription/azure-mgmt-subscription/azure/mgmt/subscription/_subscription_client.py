e33d5db67366cb6b386b3b1348f6527386f3ce1a
SDKAuto <sdkautomation@microsoft.com>
2022-05-29 04:09:13 +0000

    CodeGen from PR 19261 in Azure/azure-rest-api-specs Merge 3fd6de34974e2ca5421d474fd9af9ba31fa4a3db into 34883b2138e7a02f739e16dc78bfa551d0da46ea

--- sdk/subscription/azure-mgmt-subscription/azure/mgmt/subscription/_subscription_client.py
+++ sdk/subscription/azure-mgmt-subscription/azure/mgmt/subscription/_subscription_client.py
@@ -7,11 +7,12 @@
 # --------------------------------------------------------------------------
 
 from copy import deepcopy
-from typing import Any, Optional, TYPE_CHECKING
+from typing import Any, TYPE_CHECKING
+
+from msrest import Deserializer, Serializer
 
 from azure.core.rest import HttpRequest, HttpResponse
 from azure.mgmt.core import ARMPipelineClient
-from msrest import Deserializer, Serializer
 
 from . import models
 from ._configuration import SubscriptionClientConfiguration
@@ -21,7 +22,7 @@ if TYPE_CHECKING:
     # pylint: disable=unused-import,ungrouped-imports
     from azure.core.credentials import TokenCredential
 
-class SubscriptionClient:
+class SubscriptionClient:    # pylint: disable=too-many-instance-attributes
     """The subscription client.
 
     :ivar subscriptions: SubscriptionsOperations operations
@@ -40,7 +41,7 @@ class SubscriptionClient:
     :vartype billing_account: azure.mgmt.subscription.operations.BillingAccountOperations
     :param credential: Credential needed for the client to connect to Azure.
     :type credential: ~azure.core.credentials.TokenCredential
-    :param base_url: Service URL. Default value is 'https://management.azure.com'.
+    :param base_url: Service URL. Default value is "https://management.azure.com".
     :type base_url: str
     :keyword int polling_interval: Default waiting time between two polls for LRO operations if no
      Retry-After header is present.
@@ -70,7 +71,7 @@ class SubscriptionClient:
 
     def _send_request(
         self,
-        request,  # type: HttpRequest
+        request: HttpRequest,
         **kwargs: Any
     ) -> HttpResponse:
         """Runs the network request through the client's chained policies.
