6bf8786c5a8310e6069d87921353663e4d31295b
SDKAuto <sdkautomation@microsoft.com>
2022-06-10 20:22:21 +0000

    CodeGen from PR 18978 in Azure/azure-rest-api-specs Merge 9aa4eadffc2721cea8138f23bd0a414bd6c3994c into e7bcafa885ef773c6309d6a8f3a65c5019df413d

--- sdk/rdbms/azure-mgmt-rdbms/azure/mgmt/rdbms/postgresql/aio/__init__.py
+++ sdk/rdbms/azure-mgmt-rdbms/azure/mgmt/rdbms/postgresql/aio/__init__.py
@@ -8,3 +8,8 @@
 
 from ._postgre_sql_management_client import PostgreSQLManagementClient
 __all__ = ['PostgreSQLManagementClient']
+
+# `._patch.py` is used for handwritten extensions to the generated code
+# Example: https://github.com/Azure/azure-sdk-for-python/blob/main/doc/dev/customize_code/how-to-patch-sdk-code.md
+from ._patch import patch_sdk
+patch_sdk()
