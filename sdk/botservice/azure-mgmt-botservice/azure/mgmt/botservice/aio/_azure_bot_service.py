ff8ad41477b703f7d70b92fb3745eab6d29f39c6
SDKAuto <sdkautomation@microsoft.com>
2022-06-16 07:58:02 +0000

    CodeGen from PR 18989 in Azure/azure-rest-api-specs Merge 6e674392587259661d65f395764663cebc683867 into c2d25c0f023edd1fea753a89d6409f3e7a9f8956

--- sdk/botservice/azure-mgmt-botservice/azure/mgmt/botservice/aio/_azure_bot_service.py
+++ sdk/botservice/azure-mgmt-botservice/azure/mgmt/botservice/aio/_azure_bot_service.py
@@ -7,11 +7,12 @@
 # --------------------------------------------------------------------------
 
 from copy import deepcopy
-from typing import Any, Awaitable, Optional, TYPE_CHECKING
+from typing import Any, Awaitable, TYPE_CHECKING
+
+from msrest import Deserializer, Serializer
 
 from azure.core.rest import AsyncHttpResponse, HttpRequest
 from azure.mgmt.core import AsyncARMPipelineClient
-from msrest import Deserializer, Serializer
 
 from .. import models
 from ._configuration import AzureBotServiceConfiguration
@@ -21,7 +22,7 @@ if TYPE_CHECKING:
     # pylint: disable=unused-import,ungrouped-imports
     from azure.core.credentials_async import AsyncTokenCredential
 
-class AzureBotService:
+class AzureBotService:    # pylint: disable=too-many-instance-attributes
     """Azure Bot Service is a platform for creating smart conversational agents.
 
     :ivar bots: BotsOperations operations
@@ -48,8 +49,11 @@ class AzureBotService:
     :type credential: ~azure.core.credentials_async.AsyncTokenCredential
     :param subscription_id: Azure Subscription ID.
     :type subscription_id: str
-    :param base_url: Service URL. Default value is 'https://management.azure.com'.
+    :param base_url: Service URL. Default value is "https://management.azure.com".
     :type base_url: str
+    :keyword api_version: Api Version. Default value is "2021-05-01-preview". Note that overriding
+     this default value may result in unsupported behavior.
+    :paramtype api_version: str
     :keyword int polling_interval: Default waiting time between two polls for LRO operations if no
      Retry-After header is present.
     """
@@ -68,15 +72,33 @@ class AzureBotService:
         self._serialize = Serializer(client_models)
         self._deserialize = Deserializer(client_models)
         self._serialize.client_side_validation = False
-        self.bots = BotsOperations(self._client, self._config, self._serialize, self._deserialize)
-        self.channels = ChannelsOperations(self._client, self._config, self._serialize, self._deserialize)
-        self.direct_line = DirectLineOperations(self._client, self._config, self._serialize, self._deserialize)
-        self.operations = Operations(self._client, self._config, self._serialize, self._deserialize)
-        self.bot_connection = BotConnectionOperations(self._client, self._config, self._serialize, self._deserialize)
-        self.host_settings = HostSettingsOperations(self._client, self._config, self._serialize, self._deserialize)
-        self.operation_results = OperationResultsOperations(self._client, self._config, self._serialize, self._deserialize)
-        self.private_endpoint_connections = PrivateEndpointConnectionsOperations(self._client, self._config, self._serialize, self._deserialize)
-        self.private_link_resources = PrivateLinkResourcesOperations(self._client, self._config, self._serialize, self._deserialize)
+        self.bots = BotsOperations(
+            self._client, self._config, self._serialize, self._deserialize
+        )
+        self.channels = ChannelsOperations(
+            self._client, self._config, self._serialize, self._deserialize
+        )
+        self.direct_line = DirectLineOperations(
+            self._client, self._config, self._serialize, self._deserialize
+        )
+        self.operations = Operations(
+            self._client, self._config, self._serialize, self._deserialize
+        )
+        self.bot_connection = BotConnectionOperations(
+            self._client, self._config, self._serialize, self._deserialize
+        )
+        self.host_settings = HostSettingsOperations(
+            self._client, self._config, self._serialize, self._deserialize
+        )
+        self.operation_results = OperationResultsOperations(
+            self._client, self._config, self._serialize, self._deserialize
+        )
+        self.private_endpoint_connections = PrivateEndpointConnectionsOperations(
+            self._client, self._config, self._serialize, self._deserialize
+        )
+        self.private_link_resources = PrivateLinkResourcesOperations(
+            self._client, self._config, self._serialize, self._deserialize
+        )
 
 
     def _send_request(
--- sdk/botservice/azure-mgmt-botservice/azure/mgmt/botservice/models/__init__.py
+++ sdk/botservice/azure-mgmt-botservice/azure/mgmt/botservice/models/__init__.py
@@ -90,7 +90,9 @@ from ._azure_bot_service_enums import (
     SkuName,
     SkuTier,
 )
-
+from ._patch import __all__ as _patch_all
+from ._patch import *  # type: ignore # pylint: disable=unused-wildcard-import
+from ._patch import patch_sdk as _patch_sdk
 __all__ = [
     'AlexaChannel',
     'AlexaChannelProperties',
@@ -173,3 +175,5 @@ __all__ = [
     'SkuName',
     'SkuTier',
 ]
+__all__.extend([p for p in _patch_all if p not in __all__])
+_patch_sdk()
