e584331e4715ed6a6bcdd30b99ef01da464069a9
SDKAuto <sdkautomation@microsoft.com>
2022-06-20 07:27:01 +0000

    CodeGen from PR 19527 in Azure/azure-rest-api-specs Merge 63c8fd1b6c0b7ab9c9ecad8373570e778b81c7e3 into f4e5ae6e47d3aaaf7a7760924a81d119f740dd30

--- sdk/cdn/azure-mgmt-cdn/azure/mgmt/cdn/models/__init__.py
+++ sdk/cdn/azure-mgmt-cdn/azure/mgmt/cdn/models/__init__.py
@@ -353,7 +353,9 @@ from ._cdn_management_client_enums import (
     WafRankingType,
     WafRuleType,
 )
-
+from ._patch import __all__ as _patch_all
+from ._patch import *  # type: ignore # pylint: disable=unused-wildcard-import
+from ._patch import patch_sdk as _patch_sdk
 __all__ = [
     'AFDDomain',
     'AFDDomainHttpsParameters',
@@ -699,3 +701,5 @@ __all__ = [
     'WafRankingType',
     'WafRuleType',
 ]
+__all__.extend([p for p in _patch_all if p not in __all__])
+_patch_sdk()
