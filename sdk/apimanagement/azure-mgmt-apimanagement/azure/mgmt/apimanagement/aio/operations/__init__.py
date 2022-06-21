6fb9033cefc537255e9a336b3bfb592f62687838
SDKAuto <sdkautomation@microsoft.com>
2022-06-21 17:20:29 +0000

    CodeGen from PR 19469 in Azure/azure-rest-api-specs Merge 2b34d9a012864a8e3fbc64c34544454fa8458831 into f68e478437a9065651c982788769337fc4d8fb37

--- sdk/apimanagement/azure-mgmt-apimanagement/azure/mgmt/apimanagement/aio/operations/__init__.py
+++ sdk/apimanagement/azure-mgmt-apimanagement/azure/mgmt/apimanagement/aio/operations/__init__.py
@@ -83,6 +83,9 @@ from ._user_subscription_operations import UserSubscriptionOperations
 from ._user_identities_operations import UserIdentitiesOperations
 from ._user_confirmation_password_operations import UserConfirmationPasswordOperations
 
+from ._patch import __all__ as _patch_all
+from ._patch import *  # type: ignore # pylint: disable=unused-wildcard-import
+from ._patch import patch_sdk as _patch_sdk
 __all__ = [
     'ApiOperations',
     'ApiRevisionOperations',
@@ -161,3 +164,5 @@ __all__ = [
     'UserIdentitiesOperations',
     'UserConfirmationPasswordOperations',
 ]
+__all__.extend([p for p in _patch_all if p not in __all__])
+_patch_sdk()
