2382fda992eb863f93a3e238a80badd3baa6c6ce
SDKAuto <sdkautomation@microsoft.com>
2022-06-24 08:41:48 +0000

    CodeGen from PR 19583 in Azure/azure-rest-api-specs Merge ccb4a696c75249ff5856f6b3d9cd0792460d4734 into e8c0c73d5f11f9f2b7cde8e1ac33638bb3c5695d

--- sdk/appcontainers/azure-mgmt-appcontainers/azure/mgmt/appcontainers/models/__init__.py
+++ sdk/appcontainers/azure-mgmt-appcontainers/azure/mgmt/appcontainers/models/__init__.py
@@ -137,7 +137,9 @@ from ._container_apps_api_client_enums import (
     Type,
     UnauthenticatedClientActionV2,
 )
-
+from ._patch import __all__ as _patch_all
+from ._patch import *  # type: ignore # pylint: disable=unused-wildcard-import
+from ._patch import patch_sdk as _patch_sdk
 __all__ = [
     'AllowedAudiencesValidation',
     'AllowedPrincipals',
@@ -267,3 +269,5 @@ __all__ = [
     'Type',
     'UnauthenticatedClientActionV2',
 ]
+__all__.extend([p for p in _patch_all if p not in __all__])
+_patch_sdk()
