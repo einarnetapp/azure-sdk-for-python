1bf10791e8b0b2898f1b016c702e98548e6e872d
SDKAuto <sdkautomation@microsoft.com>
2022-06-27 11:17:26 +0000

    CodeGen from PR 19602 in Azure/azure-rest-api-specs Merge b361a4d84475f045834f1ad1d9d77c82c1928541 into 6c065f8439e6d9cd02bf4f8268fae7f86a5f0fa9

--- sdk/cosmos/azure-mgmt-cosmosdb/azure/mgmt/cosmosdb/models/__init__.py
+++ sdk/cosmos/azure-mgmt-cosmosdb/azure/mgmt/cosmosdb/models/__init__.py
@@ -8,6 +8,7 @@
 
 from ._models_py3 import ARMProxyResource
 from ._models_py3 import ARMResourceProperties
+from ._models_py3 import AccountKeyMetadata
 from ._models_py3 import AnalyticalStorageConfiguration
 from ._models_py3 import ApiProperties
 from ._models_py3 import AuthenticationMethodLdapProperties
@@ -86,6 +87,7 @@ from ._models_py3 import DataTransferServiceResourceProperties
 from ._models_py3 import DatabaseAccountConnectionString
 from ._models_py3 import DatabaseAccountCreateUpdateParameters
 from ._models_py3 import DatabaseAccountGetResults
+from ._models_py3 import DatabaseAccountKeysMetadata
 from ._models_py3 import DatabaseAccountListConnectionStringsResult
 from ._models_py3 import DatabaseAccountListKeysResult
 from ._models_py3 import DatabaseAccountListReadOnlyKeysResult
@@ -216,6 +218,7 @@ from ._models_py3 import RestorableGremlinDatabasesListResult
 from ._models_py3 import RestorableGremlinGraphGetResult
 from ._models_py3 import RestorableGremlinGraphPropertiesResource
 from ._models_py3 import RestorableGremlinGraphsListResult
+from ._models_py3 import RestorableGremlinResourcesGetResult
 from ._models_py3 import RestorableGremlinResourcesListResult
 from ._models_py3 import RestorableLocationResource
 from ._models_py3 import RestorableMongodbCollectionGetResult
@@ -236,6 +239,7 @@ from ._models_py3 import RestorableSqlDatabasesListResult
 from ._models_py3 import RestorableSqlResourcesListResult
 from ._models_py3 import RestorableTableGetResult
 from ._models_py3 import RestorableTablePropertiesResource
+from ._models_py3 import RestorableTableResourcesGetResult
 from ._models_py3 import RestorableTableResourcesListResult
 from ._models_py3 import RestorableTablesListResult
 from ._models_py3 import RestoreParameters
@@ -349,10 +353,13 @@ from ._cosmos_db_management_client_enums import (
     TriggerType,
     UnitType,
 )
-
+from ._patch import __all__ as _patch_all
+from ._patch import *  # type: ignore # pylint: disable=unused-wildcard-import
+from ._patch import patch_sdk as _patch_sdk
 __all__ = [
     'ARMProxyResource',
     'ARMResourceProperties',
+    'AccountKeyMetadata',
     'AnalyticalStorageConfiguration',
     'ApiProperties',
     'AuthenticationMethodLdapProperties',
@@ -431,6 +438,7 @@ __all__ = [
     'DatabaseAccountConnectionString',
     'DatabaseAccountCreateUpdateParameters',
     'DatabaseAccountGetResults',
+    'DatabaseAccountKeysMetadata',
     'DatabaseAccountListConnectionStringsResult',
     'DatabaseAccountListKeysResult',
     'DatabaseAccountListReadOnlyKeysResult',
@@ -561,6 +569,7 @@ __all__ = [
     'RestorableGremlinGraphGetResult',
     'RestorableGremlinGraphPropertiesResource',
     'RestorableGremlinGraphsListResult',
+    'RestorableGremlinResourcesGetResult',
     'RestorableGremlinResourcesListResult',
     'RestorableLocationResource',
     'RestorableMongodbCollectionGetResult',
@@ -581,6 +590,7 @@ __all__ = [
     'RestorableSqlResourcesListResult',
     'RestorableTableGetResult',
     'RestorableTablePropertiesResource',
+    'RestorableTableResourcesGetResult',
     'RestorableTableResourcesListResult',
     'RestorableTablesListResult',
     'RestoreParameters',
@@ -691,3 +701,5 @@ __all__ = [
     'TriggerType',
     'UnitType',
 ]
+__all__.extend([p for p in _patch_all if p not in __all__])
+_patch_sdk()
