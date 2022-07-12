d88b710aef762fb02daf7138c614f7257e05f248
SDKAuto <sdkautomation@microsoft.com>
2022-07-12 05:20:58 +0000

    CodeGen from PR 19716 in Azure/azure-rest-api-specs Merge b694193635220431592e54aea59137a812db4886 into 432872fac1d0f8edcae98a0e8504afc0ee302710

--- sdk/rdbms/azure-mgmt-rdbms/azure/mgmt/rdbms/mariadb/aio/operations/__init__.py
+++ sdk/rdbms/azure-mgmt-rdbms/azure/mgmt/rdbms/mariadb/aio/operations/__init__.py
@@ -31,6 +31,9 @@ from ._private_endpoint_connections_operations import PrivateEndpointConnections
 from ._private_link_resources_operations import PrivateLinkResourcesOperations
 from ._server_security_alert_policies_operations import ServerSecurityAlertPoliciesOperations
 
+from ._patch import __all__ as _patch_all
+from ._patch import *  # type: ignore # pylint: disable=unused-wildcard-import
+from ._patch import patch_sdk as _patch_sdk
 __all__ = [
     'ServersOperations',
     'ReplicasOperations',
@@ -57,3 +60,5 @@ __all__ = [
     'PrivateLinkResourcesOperations',
     'ServerSecurityAlertPoliciesOperations',
 ]
+__all__.extend([p for p in _patch_all if p not in __all__])
+_patch_sdk()
