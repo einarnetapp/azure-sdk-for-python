3a794faf1dffcefd20905c1d6420babdb041c57d
SDKAuto <sdkautomation@microsoft.com>
2022-07-29 21:02:32 +0000

    CodeGen from PR 20010 in Azure/azure-rest-api-specs Merge ac1df1177ae3a23e8f40a381d563eab21f413900 into eeb2cff50119cfa7b724ac63c768d637ec01827a

--- sdk/redisenterprise/azure-mgmt-redisenterprise/azure/mgmt/redisenterprise/operations/__init__.py
+++ sdk/redisenterprise/azure-mgmt-redisenterprise/azure/mgmt/redisenterprise/operations/__init__.py
@@ -12,7 +12,11 @@ from ._redis_enterprise_operations import RedisEnterpriseOperations
 from ._databases_operations import DatabasesOperations
 from ._private_endpoint_connections_operations import PrivateEndpointConnectionsOperations
 from ._private_link_resources_operations import PrivateLinkResourcesOperations
+from ._skus_operations import SkusOperations
 
+from ._patch import __all__ as _patch_all
+from ._patch import *  # type: ignore # pylint: disable=unused-wildcard-import
+from ._patch import patch_sdk as _patch_sdk
 __all__ = [
     'Operations',
     'OperationsStatusOperations',
@@ -20,4 +24,7 @@ __all__ = [
     'DatabasesOperations',
     'PrivateEndpointConnectionsOperations',
     'PrivateLinkResourcesOperations',
+    'SkusOperations',
 ]
+__all__.extend([p for p in _patch_all if p not in __all__])
+_patch_sdk()
