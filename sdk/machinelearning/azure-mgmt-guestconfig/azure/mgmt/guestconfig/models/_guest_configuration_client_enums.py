2a1f8ac36796f50f3bf40c853dad996995cbf635
SDKAuto <sdkautomation@microsoft.com>
2022-08-08 09:58:26 +0000

    CodeGen from PR 2607 in test-repo-billy/azure-rest-api-specs Merge 40743ef53b1879fc876ad5708d5898a8d1d6a24a into f840c84013f12b701aac7065ceeb13a59b26051b

--- sdk/machinelearning/azure-mgmt-guestconfig/azure/mgmt/guestconfig/models/_guest_configuration_client_enums.py
+++ sdk/machinelearning/azure-mgmt-guestconfig/azure/mgmt/guestconfig/models/_guest_configuration_client_enums.py
@@ -6,27 +6,11 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 
-from enum import Enum, EnumMeta
-from six import with_metaclass
-
-class _CaseInsensitiveEnumMeta(EnumMeta):
-    def __getitem__(self, name):
-        return super().__getitem__(name.upper())
-
-    def __getattr__(cls, name):
-        """Return the enum member matching `name`
-        We use __getattr__ instead of descriptors or inserting into the enum
-        class' __dict__ in order to support `name` and `value` being both
-        properties for enum members (which live in the class' __dict__) and
-        enum members themselves.
-        """
-        try:
-            return cls._member_map_[name.upper()]
-        except KeyError:
-            raise AttributeError(name)
+from enum import Enum
+from azure.core import CaseInsensitiveEnumMeta
 
 
-class ActionAfterReboot(with_metaclass(_CaseInsensitiveEnumMeta, str, Enum)):
+class ActionAfterReboot(str, Enum, metaclass=CaseInsensitiveEnumMeta):
     """Specifies what happens after a reboot during the application of a configuration. The possible
     values are ContinueConfiguration and StopConfiguration
     """
@@ -34,7 +18,7 @@ class ActionAfterReboot(with_metaclass(_CaseInsensitiveEnumMeta, str, Enum)):
     CONTINUE_CONFIGURATION = "ContinueConfiguration"
     STOP_CONFIGURATION = "StopConfiguration"
 
-class AssignmentType(with_metaclass(_CaseInsensitiveEnumMeta, str, Enum)):
+class AssignmentType(str, Enum, metaclass=CaseInsensitiveEnumMeta):
     """Specifies the assignment type and execution of the configuration. Possible values are Audit,
     DeployAndAutoCorrect, ApplyAndAutoCorrect and ApplyAndMonitor.
     """
@@ -44,7 +28,7 @@ class AssignmentType(with_metaclass(_CaseInsensitiveEnumMeta, str, Enum)):
     APPLY_AND_AUTO_CORRECT = "ApplyAndAutoCorrect"
     APPLY_AND_MONITOR = "ApplyAndMonitor"
 
-class ComplianceStatus(with_metaclass(_CaseInsensitiveEnumMeta, str, Enum)):
+class ComplianceStatus(str, Enum, metaclass=CaseInsensitiveEnumMeta):
     """A value indicating compliance status of the machine for the assigned guest configuration.
     """
 
@@ -52,7 +36,7 @@ class ComplianceStatus(with_metaclass(_CaseInsensitiveEnumMeta, str, Enum)):
     NON_COMPLIANT = "NonCompliant"
     PENDING = "Pending"
 
-class ConfigurationMode(with_metaclass(_CaseInsensitiveEnumMeta, str, Enum)):
+class ConfigurationMode(str, Enum, metaclass=CaseInsensitiveEnumMeta):
     """Specifies how the LCM(Local Configuration Manager) actually applies the configuration to the
     target nodes. Possible values are ApplyOnly, ApplyAndMonitor, and ApplyAndAutoCorrect.
     """
@@ -61,13 +45,22 @@ class ConfigurationMode(with_metaclass(_CaseInsensitiveEnumMeta, str, Enum)):
     APPLY_AND_MONITOR = "ApplyAndMonitor"
     APPLY_AND_AUTO_CORRECT = "ApplyAndAutoCorrect"
 
-class Kind(with_metaclass(_CaseInsensitiveEnumMeta, str, Enum)):
+class CreatedByType(str, Enum, metaclass=CaseInsensitiveEnumMeta):
+    """The type of identity that created the resource.
+    """
+
+    USER = "User"
+    APPLICATION = "Application"
+    MANAGED_IDENTITY = "ManagedIdentity"
+    KEY = "Key"
+
+class Kind(str, Enum, metaclass=CaseInsensitiveEnumMeta):
     """Kind of the guest configuration. For example:DSC
     """
 
     DSC = "DSC"
 
-class ProvisioningState(with_metaclass(_CaseInsensitiveEnumMeta, str, Enum)):
+class ProvisioningState(str, Enum, metaclass=CaseInsensitiveEnumMeta):
     """The provisioning state, which only appears in the response.
     """
 
@@ -76,7 +69,7 @@ class ProvisioningState(with_metaclass(_CaseInsensitiveEnumMeta, str, Enum)):
     CANCELED = "Canceled"
     CREATED = "Created"
 
-class Type(with_metaclass(_CaseInsensitiveEnumMeta, str, Enum)):
+class Type(str, Enum, metaclass=CaseInsensitiveEnumMeta):
     """Type of report, Consistency or Initial
     """
 
