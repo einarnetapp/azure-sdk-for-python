3c12687a31e334b1e9a259d98d3fbf946ddfb8c4
SDKAuto <sdkautomation@microsoft.com>
2022-08-08 07:04:06 +0000

    CodeGen from PR 2592 in test-repo-billy/azure-rest-api-specs Merge 43d0385361e04d8ed7d278fdc303b3abd0677fd2 into f840c84013f12b701aac7065ceeb13a59b26051b

--- sdk/workloads/azure-mgmt-workloads/azure/mgmt/workloads/models/__init__.py
+++ sdk/workloads/azure-mgmt-workloads/azure/mgmt/workloads/models/__init__.py
@@ -186,7 +186,9 @@ from ._workloads_client_enums import (
     WorkloadMonitorActionType,
     WorkloadMonitorProvisioningState,
 )
-
+from ._patch import __all__ as _patch_all
+from ._patch import *  # type: ignore # pylint: disable=unused-wildcard-import
+from ._patch import patch_sdk as _patch_sdk
 __all__ = [
     'ApplicationServerConfiguration',
     'BackupProfile',
@@ -365,3 +367,5 @@ __all__ = [
     'WorkloadMonitorActionType',
     'WorkloadMonitorProvisioningState',
 ]
+__all__.extend([p for p in _patch_all if p not in __all__])
+_patch_sdk()
