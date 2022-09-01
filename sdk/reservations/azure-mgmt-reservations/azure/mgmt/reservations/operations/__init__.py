abca970101d34b6d5d58a9a4040c544e82cbcd1a
SDKAuto <sdkautomation@microsoft.com>
2022-09-01 07:40:14 +0000

    CodeGen from PR 20537 in Azure/azure-rest-api-specs Merge f46a712598a92614b6a31c3b40b2e4c17450530b into aefbcc5fb18a3b33f401394ebeae01df0733c830

--- sdk/reservations/azure-mgmt-reservations/azure/mgmt/reservations/operations/__init__.py
+++ sdk/reservations/azure-mgmt-reservations/azure/mgmt/reservations/operations/__init__.py
@@ -15,13 +15,19 @@ from ._exchange_operations import ExchangeOperations
 from ._quota_operations import QuotaOperations
 from ._quota_request_status_operations import QuotaRequestStatusOperations
 
+from ._patch import __all__ as _patch_all
+from ._patch import *  # type: ignore # pylint: disable=unused-wildcard-import
+from ._patch import patch_sdk as _patch_sdk
+
 __all__ = [
-    'ReservationOperations',
-    'AzureReservationAPIOperationsMixin',
-    'ReservationOrderOperations',
-    'OperationOperations',
-    'CalculateExchangeOperations',
-    'ExchangeOperations',
-    'QuotaOperations',
-    'QuotaRequestStatusOperations',
+    "ReservationOperations",
+    "AzureReservationAPIOperationsMixin",
+    "ReservationOrderOperations",
+    "OperationOperations",
+    "CalculateExchangeOperations",
+    "ExchangeOperations",
+    "QuotaOperations",
+    "QuotaRequestStatusOperations",
 ]
+__all__.extend([p for p in _patch_all if p not in __all__])
+_patch_sdk()
