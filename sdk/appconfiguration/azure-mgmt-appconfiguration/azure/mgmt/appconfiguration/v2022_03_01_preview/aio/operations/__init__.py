94e16beaba98fc33d437362ad173fe7ec2bd44db
SDKAuto <sdkautomation@microsoft.com>
2022-09-07 16:23:58 +0000

    CodeGen from PR 20548 in Azure/azure-rest-api-specs Merge 0e21a6a5518f94513e7091ac5aa5df3ae6240b83 into 79776c8ff0fb29c7e1f43daec925a1b91ef196b0

--- sdk/appconfiguration/azure-mgmt-appconfiguration/azure/mgmt/appconfiguration/v2022_03_01_preview/aio/operations/__init__.py
+++ sdk/appconfiguration/azure-mgmt-appconfiguration/azure/mgmt/appconfiguration/v2022_03_01_preview/aio/operations/__init__.py
@@ -16,13 +16,14 @@ from ._replicas_operations import ReplicasOperations
 from ._patch import __all__ as _patch_all
 from ._patch import *  # type: ignore # pylint: disable=unused-wildcard-import
 from ._patch import patch_sdk as _patch_sdk
+
 __all__ = [
-    'ConfigurationStoresOperations',
-    'Operations',
-    'PrivateEndpointConnectionsOperations',
-    'PrivateLinkResourcesOperations',
-    'KeyValuesOperations',
-    'ReplicasOperations',
+    "ConfigurationStoresOperations",
+    "Operations",
+    "PrivateEndpointConnectionsOperations",
+    "PrivateLinkResourcesOperations",
+    "KeyValuesOperations",
+    "ReplicasOperations",
 ]
 __all__.extend([p for p in _patch_all if p not in __all__])
 _patch_sdk()
