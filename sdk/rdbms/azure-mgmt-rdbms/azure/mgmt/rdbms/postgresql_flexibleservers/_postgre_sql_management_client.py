4a99e8bfaa05bddb6af168cc16271d9782cb6819
SDKAuto <sdkautomation@microsoft.com>
2022-10-09 09:53:57 +0000

    CodeGen from PR 21042 in Azure/azure-rest-api-specs Merge 43a4cd0d410dd5ec99033f9a6ed8c3f410f92011 into 7cbec7912bba007dedec603c58086239f979216b

--- sdk/rdbms/azure-mgmt-rdbms/azure/mgmt/rdbms/postgresql_flexibleservers/_postgre_sql_management_client.py
+++ sdk/rdbms/azure-mgmt-rdbms/azure/mgmt/rdbms/postgresql_flexibleservers/_postgre_sql_management_client.py
@@ -9,33 +9,36 @@
 from copy import deepcopy
 from typing import Any, TYPE_CHECKING
 
-from msrest import Deserializer, Serializer
-
 from azure.core.rest import HttpRequest, HttpResponse
 from azure.mgmt.core import ARMPipelineClient
 
 from . import models
 from ._configuration import PostgreSQLManagementClientConfiguration
-from .operations import BackupsOperations, CheckNameAvailabilityOperations, ConfigurationsOperations, DatabasesOperations, FirewallRulesOperations, GetPrivateDnsZoneSuffixOperations, LocationBasedCapabilitiesOperations, Operations, ServersOperations, VirtualNetworkSubnetUsageOperations
+from ._serialization import Deserializer, Serializer
+from .operations import (
+    CheckNameAvailabilityOperations,
+    ConfigurationsOperations,
+    DatabasesOperations,
+    FirewallRulesOperations,
+    GetPrivateDnsZoneSuffixOperations,
+    LocationBasedCapabilitiesOperations,
+    Operations,
+    ServersOperations,
+    VirtualNetworkSubnetUsageOperations,
+)
 
 if TYPE_CHECKING:
     # pylint: disable=unused-import,ungrouped-imports
     from azure.core.credentials import TokenCredential
 
-class PostgreSQLManagementClient:    # pylint: disable=too-many-instance-attributes
+
+class PostgreSQLManagementClient:  # pylint: disable=client-accepts-api-version-keyword,too-many-instance-attributes
     """The Microsoft Azure management API provides create, read, update, and delete functionality for
     Azure PostgreSQL resources including servers, databases, firewall rules, VNET rules, security
     alert policies, log files and configurations with new business model.
 
-    :ivar databases: DatabasesOperations operations
-    :vartype databases: azure.mgmt.rdbms.postgresql_flexibleservers.operations.DatabasesOperations
-    :ivar get_private_dns_zone_suffix: GetPrivateDnsZoneSuffixOperations operations
-    :vartype get_private_dns_zone_suffix:
-     azure.mgmt.rdbms.postgresql_flexibleservers.operations.GetPrivateDnsZoneSuffixOperations
     :ivar servers: ServersOperations operations
     :vartype servers: azure.mgmt.rdbms.postgresql_flexibleservers.operations.ServersOperations
-    :ivar backups: BackupsOperations operations
-    :vartype backups: azure.mgmt.rdbms.postgresql_flexibleservers.operations.BackupsOperations
     :ivar firewall_rules: FirewallRulesOperations operations
     :vartype firewall_rules:
      azure.mgmt.rdbms.postgresql_flexibleservers.operations.FirewallRulesOperations
@@ -53,14 +56,19 @@ class PostgreSQLManagementClient:    # pylint: disable=too-many-instance-attribu
      azure.mgmt.rdbms.postgresql_flexibleservers.operations.VirtualNetworkSubnetUsageOperations
     :ivar operations: Operations operations
     :vartype operations: azure.mgmt.rdbms.postgresql_flexibleservers.operations.Operations
-    :param credential: Credential needed for the client to connect to Azure.
+    :ivar databases: DatabasesOperations operations
+    :vartype databases: azure.mgmt.rdbms.postgresql_flexibleservers.operations.DatabasesOperations
+    :ivar get_private_dns_zone_suffix: GetPrivateDnsZoneSuffixOperations operations
+    :vartype get_private_dns_zone_suffix:
+     azure.mgmt.rdbms.postgresql_flexibleservers.operations.GetPrivateDnsZoneSuffixOperations
+    :param credential: Credential needed for the client to connect to Azure. Required.
     :type credential: ~azure.core.credentials.TokenCredential
-    :param subscription_id: The ID of the target subscription.
+    :param subscription_id: The ID of the target subscription. Required.
     :type subscription_id: str
     :param base_url: Service URL. Default value is "https://management.azure.com".
     :type base_url: str
-    :keyword api_version: Api Version. Default value is "2022-01-20-preview". Note that overriding
-     this default value may result in unsupported behavior.
+    :keyword api_version: Api Version. Default value is "2021-06-01". Note that overriding this
+     default value may result in unsupported behavior.
     :paramtype api_version: str
     :keyword int polling_interval: Default waiting time between two polls for LRO operations if no
      Retry-After header is present.
@@ -73,31 +81,18 @@ class PostgreSQLManagementClient:    # pylint: disable=too-many-instance-attribu
         base_url: str = "https://management.azure.com",
         **kwargs: Any
     ) -> None:
-        self._config = PostgreSQLManagementClientConfiguration(credential=credential, subscription_id=subscription_id, **kwargs)
+        self._config = PostgreSQLManagementClientConfiguration(
+            credential=credential, subscription_id=subscription_id, **kwargs
+        )
         self._client = ARMPipelineClient(base_url=base_url, config=self._config, **kwargs)
 
         client_models = {k: v for k, v in models.__dict__.items() if isinstance(v, type)}
         self._serialize = Serializer(client_models)
         self._deserialize = Deserializer(client_models)
         self._serialize.client_side_validation = False
-        self.databases = DatabasesOperations(
-            self._client, self._config, self._serialize, self._deserialize
-        )
-        self.get_private_dns_zone_suffix = GetPrivateDnsZoneSuffixOperations(
-            self._client, self._config, self._serialize, self._deserialize
-        )
-        self.servers = ServersOperations(
-            self._client, self._config, self._serialize, self._deserialize
-        )
-        self.backups = BackupsOperations(
-            self._client, self._config, self._serialize, self._deserialize
-        )
-        self.firewall_rules = FirewallRulesOperations(
-            self._client, self._config, self._serialize, self._deserialize
-        )
-        self.configurations = ConfigurationsOperations(
-            self._client, self._config, self._serialize, self._deserialize
-        )
+        self.servers = ServersOperations(self._client, self._config, self._serialize, self._deserialize)
+        self.firewall_rules = FirewallRulesOperations(self._client, self._config, self._serialize, self._deserialize)
+        self.configurations = ConfigurationsOperations(self._client, self._config, self._serialize, self._deserialize)
         self.check_name_availability = CheckNameAvailabilityOperations(
             self._client, self._config, self._serialize, self._deserialize
         )
@@ -107,16 +102,13 @@ class PostgreSQLManagementClient:    # pylint: disable=too-many-instance-attribu
         self.virtual_network_subnet_usage = VirtualNetworkSubnetUsageOperations(
             self._client, self._config, self._serialize, self._deserialize
         )
-        self.operations = Operations(
+        self.operations = Operations(self._client, self._config, self._serialize, self._deserialize)
+        self.databases = DatabasesOperations(self._client, self._config, self._serialize, self._deserialize)
+        self.get_private_dns_zone_suffix = GetPrivateDnsZoneSuffixOperations(
             self._client, self._config, self._serialize, self._deserialize
         )
 
-
-    def _send_request(
-        self,
-        request: HttpRequest,
-        **kwargs: Any
-    ) -> HttpResponse:
+    def _send_request(self, request: HttpRequest, **kwargs: Any) -> HttpResponse:
         """Runs the network request through the client's chained policies.
 
         >>> from azure.core.rest import HttpRequest
@@ -125,7 +117,7 @@ class PostgreSQLManagementClient:    # pylint: disable=too-many-instance-attribu
         >>> response = client._send_request(request)
         <HttpResponse: 200 OK>
 
-        For more information on this code flow, see https://aka.ms/azsdk/python/protocol/quickstart
+        For more information on this code flow, see https://aka.ms/azsdk/dpcodegen/python/send_request
 
         :param request: The network request you want to make. Required.
         :type request: ~azure.core.rest.HttpRequest
