cc23944208cecb04252137bc3b45855de6b68362
SDKAuto <sdkautomation@microsoft.com>
2022-10-26 07:07:04 +0000

    CodeGen from PR 21241 in Azure/azure-rest-api-specs Merge fd4ec2598c5cf48b07c587ae4fd59ecd7088f459 into faee3243abfa33a13a2c0c59f2d9285257f4b161

--- sdk/applicationinsights/azure-mgmt-applicationinsights/azure/mgmt/applicationinsights/v2020_02_02_preview/_application_insights_management_client.py
+++ sdk/applicationinsights/azure-mgmt-applicationinsights/azure/mgmt/applicationinsights/v2020_02_02_preview/_application_insights_management_client.py
@@ -9,12 +9,11 @@
 from copy import deepcopy
 from typing import Any, TYPE_CHECKING
 
-from msrest import Deserializer, Serializer
-
 from azure.core.rest import HttpRequest, HttpResponse
 from azure.mgmt.core import ARMPipelineClient
 
 from . import models
+from .._serialization import Deserializer, Serializer
 from ._configuration import ApplicationInsightsManagementClientConfiguration
 from .operations import ComponentsOperations
 
@@ -22,15 +21,16 @@ if TYPE_CHECKING:
     # pylint: disable=unused-import,ungrouped-imports
     from azure.core.credentials import TokenCredential
 
-class ApplicationInsightsManagementClient:
+
+class ApplicationInsightsManagementClient:  # pylint: disable=client-accepts-api-version-keyword
     """Composite Swagger for Application Insights Management Client.
 
     :ivar components: ComponentsOperations operations
     :vartype components:
      azure.mgmt.applicationinsights.v2020_02_02_preview.operations.ComponentsOperations
-    :param credential: Credential needed for the client to connect to Azure.
+    :param credential: Credential needed for the client to connect to Azure. Required.
     :type credential: ~azure.core.credentials.TokenCredential
-    :param subscription_id: The ID of the target subscription.
+    :param subscription_id: The ID of the target subscription. Required.
     :type subscription_id: str
     :param base_url: Service URL. Default value is "https://management.azure.com".
     :type base_url: str
@@ -46,7 +46,9 @@ class ApplicationInsightsManagementClient:
         base_url: str = "https://management.azure.com",
         **kwargs: Any
     ) -> None:
-        self._config = ApplicationInsightsManagementClientConfiguration(credential=credential, subscription_id=subscription_id, **kwargs)
+        self._config = ApplicationInsightsManagementClientConfiguration(
+            credential=credential, subscription_id=subscription_id, **kwargs
+        )
         self._client = ARMPipelineClient(base_url=base_url, config=self._config, **kwargs)
 
         client_models = {k: v for k, v in models.__dict__.items() if isinstance(v, type)}
@@ -55,12 +57,7 @@ class ApplicationInsightsManagementClient:
         self._serialize.client_side_validation = False
         self.components = ComponentsOperations(self._client, self._config, self._serialize, self._deserialize)
 
-
-    def _send_request(
-        self,
-        request: HttpRequest,
-        **kwargs: Any
-    ) -> HttpResponse:
+    def _send_request(self, request: HttpRequest, **kwargs: Any) -> HttpResponse:
         """Runs the network request through the client's chained policies.
 
         >>> from azure.core.rest import HttpRequest
@@ -69,7 +66,7 @@ class ApplicationInsightsManagementClient:
         >>> response = client._send_request(request)
         <HttpResponse: 200 OK>
 
-        For more information on this code flow, see https://aka.ms/azsdk/python/protocol/quickstart
+        For more information on this code flow, see https://aka.ms/azsdk/dpcodegen/python/send_request
 
         :param request: The network request you want to make. Required.
         :type request: ~azure.core.rest.HttpRequest
