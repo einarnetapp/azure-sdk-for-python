bfe739ae03d40378c62cc0710bb1ac87d31b3358
SDKAuto <sdkautomation@microsoft.com>
2022-11-02 03:35:12 +0000

    CodeGen from PR 21192 in Azure/azure-rest-api-specs Merge 6b413f14fed652679c2641c7ca7815f608d652f7 into 6054f1d9f96a4f5189dace33facf9144d468e705

--- sdk/storage/azure-mgmt-storagecache/azure/mgmt/storagecache/aio/operations/__init__.py
+++ sdk/storage/azure-mgmt-storagecache/azure/mgmt/storagecache/aio/operations/__init__.py
@@ -18,15 +18,16 @@ from ._storage_target_operations import StorageTargetOperations
 from ._patch import __all__ as _patch_all
 from ._patch import *  # type: ignore # pylint: disable=unused-wildcard-import
 from ._patch import patch_sdk as _patch_sdk
+
 __all__ = [
-    'Operations',
-    'SkusOperations',
-    'UsageModelsOperations',
-    'AscOperationsOperations',
-    'AscUsagesOperations',
-    'CachesOperations',
-    'StorageTargetsOperations',
-    'StorageTargetOperations',
+    "Operations",
+    "SkusOperations",
+    "UsageModelsOperations",
+    "AscOperationsOperations",
+    "AscUsagesOperations",
+    "CachesOperations",
+    "StorageTargetsOperations",
+    "StorageTargetOperations",
 ]
 __all__.extend([p for p in _patch_all if p not in __all__])
 _patch_sdk()
