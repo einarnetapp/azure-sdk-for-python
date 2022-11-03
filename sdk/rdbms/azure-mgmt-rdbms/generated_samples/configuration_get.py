06117b977b5195780670267adc24c119eacc9654
SDKAuto <sdkautomation@microsoft.com>
2022-11-03 05:34:56 +0000

    CodeGen from PR 21406 in Azure/azure-rest-api-specs Merge f664142ed22c70e3767c7f03a6c8fbeffe0648a1 into 591b17c5a50e7fc0ef09211197279e6d9f7ebc22

--- sdk/rdbms/azure-mgmt-rdbms/generated_samples/configuration_get.py
+++ sdk/rdbms/azure-mgmt-rdbms/generated_samples/configuration_get.py
@@ -7,7 +7,7 @@
 # --------------------------------------------------------------------------
 
 from azure.identity import DefaultAzureCredential
-from azure.mgmt.rdbms import PostgreSQLManagementClient
+from azure.mgmt.rdbms import MariaDBManagementClient
 
 """
 # PREREQUISITES
@@ -24,7 +24,7 @@ from azure.mgmt.rdbms import PostgreSQLManagementClient
 
 
 def main():
-    client = PostgreSQLManagementClient(
+    client = MariaDBManagementClient(
         credential=DefaultAzureCredential(),
         subscription_id="ffffffff-ffff-ffff-ffff-ffffffffffff",
     )
@@ -32,11 +32,11 @@ def main():
     response = client.configurations.get(
         resource_group_name="TestGroup",
         server_name="testserver",
-        configuration_name="array_nulls",
+        configuration_name="event_scheduler",
     )
     print(response)
 
 
-# x-ms-original-file: specification/postgresql/resource-manager/Microsoft.DBforPostgreSQL/stable/2017-12-01/examples/ConfigurationGet.json
+# x-ms-original-file: specification/mariadb/resource-manager/Microsoft.DBforMariaDB/stable/2018-06-01/examples/ConfigurationGet.json
 if __name__ == "__main__":
     main()
