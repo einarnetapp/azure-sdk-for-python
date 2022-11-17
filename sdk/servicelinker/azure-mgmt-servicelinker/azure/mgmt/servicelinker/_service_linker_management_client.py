ec3a8ca258dd9e1586a8295dca78ed450cb8fbcd
SDKAuto <sdkautomation@microsoft.com>
2022-11-17 06:29:47 +0000

    CodeGen from PR 21408 in Azure/azure-rest-api-specs Merge 23e2337a0979ad4dacc8e6ae5b8760d74e74fe48 into b32e1896f30e6ea155449cb49719a6286e32b961

--- sdk/servicelinker/azure-mgmt-servicelinker/azure/mgmt/servicelinker/_service_linker_management_client.py
+++ sdk/servicelinker/azure-mgmt-servicelinker/azure/mgmt/servicelinker/_service_linker_management_client.py
@@ -9,27 +9,27 @@
 from copy import deepcopy
 from typing import Any, TYPE_CHECKING
 
-from msrest import Deserializer, Serializer
-
 from azure.core.rest import HttpRequest, HttpResponse
 from azure.mgmt.core import ARMPipelineClient
 
-from . import models
+from . import models as _models
 from ._configuration import ServiceLinkerManagementClientConfiguration
+from ._serialization import Deserializer, Serializer
 from .operations import LinkerOperations, Operations
 
 if TYPE_CHECKING:
     # pylint: disable=unused-import,ungrouped-imports
     from azure.core.credentials import TokenCredential
 
-class ServiceLinkerManagementClient:
+
+class ServiceLinkerManagementClient:  # pylint: disable=client-accepts-api-version-keyword
     """Microsoft.ServiceLinker provider.
 
     :ivar linker: LinkerOperations operations
     :vartype linker: azure.mgmt.servicelinker.operations.LinkerOperations
     :ivar operations: Operations operations
     :vartype operations: azure.mgmt.servicelinker.operations.Operations
-    :param credential: Credential needed for the client to connect to Azure.
+    :param credential: Credential needed for the client to connect to Azure. Required.
     :type credential: ~azure.core.credentials.TokenCredential
     :param base_url: Service URL. Default value is "https://management.azure.com".
     :type base_url: str
@@ -41,27 +41,19 @@ class ServiceLinkerManagementClient:
     """
 
     def __init__(
-        self,
-        credential: "TokenCredential",
-        base_url: str = "https://management.azure.com",
-        **kwargs: Any
+        self, credential: "TokenCredential", base_url: str = "https://management.azure.com", **kwargs: Any
     ) -> None:
         self._config = ServiceLinkerManagementClientConfiguration(credential=credential, **kwargs)
         self._client = ARMPipelineClient(base_url=base_url, config=self._config, **kwargs)
 
-        client_models = {k: v for k, v in models.__dict__.items() if isinstance(v, type)}
+        client_models = {k: v for k, v in _models.__dict__.items() if isinstance(v, type)}
         self._serialize = Serializer(client_models)
         self._deserialize = Deserializer(client_models)
         self._serialize.client_side_validation = False
         self.linker = LinkerOperations(self._client, self._config, self._serialize, self._deserialize)
         self.operations = Operations(self._client, self._config, self._serialize, self._deserialize)
 
-
-    def _send_request(
-        self,
-        request: HttpRequest,
-        **kwargs: Any
-    ) -> HttpResponse:
+    def _send_request(self, request: HttpRequest, **kwargs: Any) -> HttpResponse:
         """Runs the network request through the client's chained policies.
 
         >>> from azure.core.rest import HttpRequest
@@ -70,7 +62,7 @@ class ServiceLinkerManagementClient:
         >>> response = client._send_request(request)
         <HttpResponse: 200 OK>
 
-        For more information on this code flow, see https://aka.ms/azsdk/python/protocol/quickstart
+        For more information on this code flow, see https://aka.ms/azsdk/dpcodegen/python/send_request
 
         :param request: The network request you want to make. Required.
         :type request: ~azure.core.rest.HttpRequest
@@ -83,15 +75,12 @@ class ServiceLinkerManagementClient:
         request_copy.url = self._client.format_url(request_copy.url)
         return self._client.send_request(request_copy, **kwargs)
 
-    def close(self):
-        # type: () -> None
+    def close(self) -> None:
         self._client.close()
 
-    def __enter__(self):
-        # type: () -> ServiceLinkerManagementClient
+    def __enter__(self) -> "ServiceLinkerManagementClient":
         self._client.__enter__()
         return self
 
-    def __exit__(self, *exc_details):
-        # type: (Any) -> None
+    def __exit__(self, *exc_details) -> None:
         self._client.__exit__(*exc_details)
