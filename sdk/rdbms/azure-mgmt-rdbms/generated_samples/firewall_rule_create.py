bc0d08345fbeac143aa919ece61d83179f77d4e8
SDKAuto <sdkautomation@microsoft.com>
2023-01-31 12:15:56 +0000

    CodeGen from PR 22389 in Azure/azure-rest-api-specs Merge c88b9d21c8f03dbecc7277679c6843cd4e648f4f into 255879667efd8140fae295b4bb52ab836269ee08

--- sdk/rdbms/azure-mgmt-rdbms/generated_samples/firewall_rule_create.py
+++ sdk/rdbms/azure-mgmt-rdbms/generated_samples/firewall_rule_create.py
@@ -7,7 +7,7 @@
 # --------------------------------------------------------------------------
 
 from azure.identity import DefaultAzureCredential
-from azure.mgmt.rdbms import PostgreSQLManagementClient
+from azure.mgmt.rdbms import CosmosDBForPostgreSQL
 
 """
 # PREREQUISITES
@@ -24,20 +24,20 @@ from azure.mgmt.rdbms import PostgreSQLManagementClient
 
 
 def main():
-    client = PostgreSQLManagementClient(
+    client = CosmosDBForPostgreSQL(
         credential=DefaultAzureCredential(),
         subscription_id="ffffffff-ffff-ffff-ffff-ffffffffffff",
     )
 
     response = client.firewall_rules.begin_create_or_update(
         resource_group_name="TestGroup",
-        server_name="testserver",
+        cluster_name="pgtestsvc4",
         firewall_rule_name="rule1",
         parameters={"properties": {"endIpAddress": "255.255.255.255", "startIpAddress": "0.0.0.0"}},
     ).result()
     print(response)
 
 
-# x-ms-original-file: specification/postgresql/resource-manager/Microsoft.DBforPostgreSQL/stable/2017-12-01/examples/FirewallRuleCreate.json
+# x-ms-original-file: specification/postgresqlhsc/resource-manager/Microsoft.DBforPostgreSQL/stable/2022-11-08/examples/FirewallRuleCreate.json
 if __name__ == "__main__":
     main()
