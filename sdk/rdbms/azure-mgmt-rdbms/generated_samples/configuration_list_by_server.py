7e7a7edcd6fbc70a9150a815ad9a611892f75281
SDKAuto <sdkautomation@microsoft.com>
2023-02-01 13:19:53 +0000

    CodeGen from PR 22408 in Azure/azure-rest-api-specs Merge 92fa4b3f928009536a5ca6b439748f629b915cad into 7fd77d2b8c1957a52e950eeb838fa742c0829163

--- sdk/rdbms/azure-mgmt-rdbms/generated_samples/configuration_list_by_server.py
+++ sdk/rdbms/azure-mgmt-rdbms/generated_samples/configuration_list_by_server.py
@@ -7,7 +7,7 @@
 # --------------------------------------------------------------------------
 
 from azure.identity import DefaultAzureCredential
-from azure.mgmt.rdbms import PostgreSQLManagementClient
+from azure.mgmt.rdbms import CosmosDBForPostgreSQL
 
 """
 # PREREQUISITES
@@ -24,19 +24,20 @@ from azure.mgmt.rdbms import PostgreSQLManagementClient
 
 
 def main():
-    client = PostgreSQLManagementClient(
+    client = CosmosDBForPostgreSQL(
         credential=DefaultAzureCredential(),
         subscription_id="ffffffff-ffff-ffff-ffff-ffffffffffff",
     )
 
     response = client.configurations.list_by_server(
-        resource_group_name="TestGroup",
+        resource_group_name="TestResourceGroup",
+        cluster_name="hsctestsg",
         server_name="testserver",
     )
     for item in response:
         print(item)
 
 
-# x-ms-original-file: specification/postgresql/resource-manager/Microsoft.DBforPostgreSQL/stable/2017-12-01/examples/ConfigurationListByServer.json
+# x-ms-original-file: specification/postgresqlhsc/resource-manager/Microsoft.DBforPostgreSQL/stable/2022-11-08/examples/ConfigurationListByServer.json
 if __name__ == "__main__":
     main()
