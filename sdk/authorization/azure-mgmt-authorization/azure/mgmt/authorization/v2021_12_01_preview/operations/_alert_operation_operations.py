d26b8ee10c1ad7e8ff6031caebdafa6f85c50f2d
SDKAuto <sdkautomation@microsoft.com>
2023-02-16 14:25:06 +0000

    CodeGen from PR 22642 in Azure/azure-rest-api-specs Merge 1dfbc16cd0844aeb91cd1cbb274bd94455711fce into 3d8685062f830eae770780a43a9a656f155412f3

--- sdk/authorization/azure-mgmt-authorization/azure/mgmt/authorization/v2021_12_01_preview/operations/_alert_operation_operations.py
+++ sdk/authorization/azure-mgmt-authorization/azure/mgmt/authorization/v2021_12_01_preview/operations/_alert_operation_operations.py
@@ -68,32 +68,6 @@ def build_get_request(scope: str, operation_id: str, **kwargs: Any) -> HttpReque
     return HttpRequest(method="GET", url=_url, params=_params, headers=_headers, **kwargs)
 
 
-def build_list_for_scope_request(scope: str, **kwargs: Any) -> HttpRequest:
-    _headers = case_insensitive_dict(kwargs.pop("headers", {}) or {})
-    _params = case_insensitive_dict(kwargs.pop("params", {}) or {})
-
-    api_version: Literal["2022-08-01-preview"] = kwargs.pop(
-        "api_version", _params.pop("api-version", "2022-08-01-preview")
-    )
-    accept = _headers.pop("Accept", "application/json")
-
-    # Construct URL
-    _url = kwargs.pop("template_url", "/{scope}/providers/Microsoft.Authorization/roleManagementAlertOperations")
-    path_format_arguments = {
-        "scope": _SERIALIZER.url("scope", scope, "str", skip_quote=True),
-    }
-
-    _url: str = _format_url_section(_url, **path_format_arguments)  # type: ignore
-
-    # Construct parameters
-    _params["api-version"] = _SERIALIZER.query("api_version", api_version, "str")
-
-    # Construct headers
-    _headers["Accept"] = _SERIALIZER.header("accept", accept, "str")
-
-    return HttpRequest(method="GET", url=_url, params=_params, headers=_headers, **kwargs)
-
-
 class AlertOperationOperations:
     """
     .. warning::
@@ -171,59 +145,3 @@ class AlertOperationOperations:
         return deserialized
 
     get.metadata = {"url": "/{scope}/providers/Microsoft.Authorization/roleManagementAlertOperations/{operationId}"}
-
-    @distributed_trace
-    def list_for_scope(self, scope: str, **kwargs: Any) -> _models.AlertOperationListResult:
-        """Gets alert operations for a resource scope.
-
-        :param scope: The scope of the alert operation. Required.
-        :type scope: str
-        :keyword callable cls: A custom type or function that will be passed the direct response
-        :return: AlertOperationListResult or the result of cls(response)
-        :rtype: ~azure.mgmt.authorization.v2021_12_01_preview.models.AlertOperationListResult
-        :raises ~azure.core.exceptions.HttpResponseError:
-        """
-        error_map = {
-            401: ClientAuthenticationError,
-            404: ResourceNotFoundError,
-            409: ResourceExistsError,
-            304: ResourceNotModifiedError,
-        }
-        error_map.update(kwargs.pop("error_map", {}) or {})
-
-        _headers = kwargs.pop("headers", {}) or {}
-        _params = case_insensitive_dict(kwargs.pop("params", {}) or {})
-
-        api_version: Literal["2022-08-01-preview"] = kwargs.pop(
-            "api_version", _params.pop("api-version", "2022-08-01-preview")
-        )
-        cls: ClsType[_models.AlertOperationListResult] = kwargs.pop("cls", None)
-
-        request = build_list_for_scope_request(
-            scope=scope,
-            api_version=api_version,
-            template_url=self.list_for_scope.metadata["url"],
-            headers=_headers,
-            params=_params,
-        )
-        request = _convert_request(request)
-        request.url = self._client.format_url(request.url)
-
-        pipeline_response: PipelineResponse = self._client._pipeline.run(  # pylint: disable=protected-access
-            request, stream=False, **kwargs
-        )
-
-        response = pipeline_response.http_response
-
-        if response.status_code not in [200]:
-            map_error(status_code=response.status_code, response=response, error_map=error_map)
-            raise HttpResponseError(response=response, error_format=ARMErrorFormat)
-
-        deserialized = self._deserialize("AlertOperationListResult", pipeline_response)
-
-        if cls:
-            return cls(pipeline_response, deserialized, {})
-
-        return deserialized
-
-    list_for_scope.metadata = {"url": "/{scope}/providers/Microsoft.Authorization/roleManagementAlertOperations"}
