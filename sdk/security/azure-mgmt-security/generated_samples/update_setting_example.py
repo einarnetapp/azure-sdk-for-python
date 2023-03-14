e001eeca59ace4d3ea49011b08c3422c08eeaaf2
SDKAuto <sdkautomation@microsoft.com>
2023-03-14 11:47:55 +0000

    CodeGen from PR 23072 in Azure/azure-rest-api-specs Merge b8c46b0bed5d6c7a8f17835b73e302bb441601cc into f858b937d0f921a9d008a1d03ab74c3fc4378470

--- sdk/security/azure-mgmt-security/generated_samples/update_setting_example.py
+++ sdk/security/azure-mgmt-security/generated_samples/update_setting_example.py
@@ -30,7 +30,7 @@ def main():
     )
 
     response = client.settings.update(
-        setting_name="MCAS",
+        setting_name="WDATP",
         setting={"kind": "DataExportSettings", "properties": {"enabled": True}},
     )
     print(response)
