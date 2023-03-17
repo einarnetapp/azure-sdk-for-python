82c66dbd30e79dac50854ed9160872e428dc9962
SDKAuto <sdkautomation@microsoft.com>
2023-03-17 07:32:24 +0000

    CodeGen from PR 23029 in Azure/azure-rest-api-specs Merge b57752f3839dd8ff9e2c88183e3ee7652ada7c44 into fce14bf881523f16b14d8ecf325e004465cdcf56

--- sdk/cdn/azure-mgmt-cdn/generated_samples/afd_custom_domains_refresh_validation_token.py
+++ sdk/cdn/azure-mgmt-cdn/generated_samples/afd_custom_domains_refresh_validation_token.py
@@ -14,9 +14,9 @@ from azure.mgmt.cdn import CdnManagementClient
     pip install azure-identity
     pip install azure-mgmt-cdn
 # USAGE
-    python delete_protection_policy.py
+    python afd_custom_domains_refresh_validation_token.py
 
-    Before run the sample, please set the values of the client ID, tenant ID and client secret 
+    Before run the sample, please set the values of the client ID, tenant ID and client secret
     of the AAD application as environment variables: AZURE_CLIENT_ID, AZURE_TENANT_ID,
     AZURE_CLIENT_SECRET. For more info about how to get the value, please see:
     https://docs.microsoft.com/azure/active-directory/develop/howto-create-service-principal-portal
@@ -29,13 +29,14 @@ def main():
         subscription_id="subid",
     )
 
-    response = client.policies.delete(
-        resource_group_name="rg1",
-        policy_name="Policy1",
-    )
+    response = client.afd_custom_domains.begin_refresh_validation_token(
+        resource_group_name="RG",
+        profile_name="profile1",
+        custom_domain_name="domain1",
+    ).result()
     print(response)
 
 
-# x-ms-original-file: specification/cdn/resource-manager/Microsoft.Cdn/stable/2021-06-01/examples/WafPolicyDelete.json
+# x-ms-original-file: specification/cdn/resource-manager/Microsoft.Cdn/preview/2022-11-01-preview/examples/AFDCustomDomains_RefreshValidationToken.json
 if __name__ == "__main__":
     main()
