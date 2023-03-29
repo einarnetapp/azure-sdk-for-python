57e22e3d6f3fd4a95d70c64951d82285a26f9ee5
SDKAuto <sdkautomation@microsoft.com>
2023-03-29 21:07:47 +0000

    CodeGen from PR 23363 in Azure/azure-rest-api-specs Merge c98c54413d3a146c10c3280cb3676c47b2ce43f6 into e5d60da002b234ddaf61d8a4176a832f9d2b14ea

--- sdk/commerce/azure-mgmt-commerce/azure/mgmt/commerce/aio/_usage_management_client.py
+++ sdk/commerce/azure-mgmt-commerce/azure/mgmt/commerce/aio/_usage_management_client.py
@@ -51,7 +51,7 @@ class UsageManagementClient:  # pylint: disable=client-accepts-api-version-keywo
         self._config = UsageManagementClientConfiguration(
             credential=credential, subscription_id=subscription_id, **kwargs
         )
-        self._client = AsyncARMPipelineClient(base_url=base_url, config=self._config, **kwargs)
+        self._client: AsyncARMPipelineClient = AsyncARMPipelineClient(base_url=base_url, config=self._config, **kwargs)
 
         client_models = {k: v for k, v in _models.__dict__.items() if isinstance(v, type)}
         self._serialize = Serializer(client_models)
--- sdk/commerce/azure-mgmt-commerce/azure/mgmt/commerce/operations/_usage_aggregates_operations.py
+++ sdk/commerce/azure-mgmt-commerce/azure/mgmt/commerce/operations/_usage_aggregates_operations.py
@@ -7,7 +7,6 @@
 # Changes may cause incorrect behavior and will be lost if the code is regenerated.
 # --------------------------------------------------------------------------
 import datetime
-import sys
 from typing import Any, Callable, Dict, Iterable, Optional, TypeVar, Union
 import urllib.parse
 
@@ -31,10 +30,6 @@ from .. import models as _models
 from .._serialization import Serializer
 from .._vendor import _convert_request, _format_url_section
 
-if sys.version_info >= (3, 8):
-    from typing import Literal  # pylint: disable=no-name-in-module, ungrouped-imports
-else:
-    from typing_extensions import Literal  # type: ignore  # pylint: disable=ungrouped-imports
 T = TypeVar("T")
 ClsType = Optional[Callable[[PipelineResponse[HttpRequest, HttpResponse], T, Dict[str, Any]], Any]]
 
@@ -55,9 +50,7 @@ def build_list_request(
     _headers = case_insensitive_dict(kwargs.pop("headers", {}) or {})
     _params = case_insensitive_dict(kwargs.pop("params", {}) or {})
 
-    api_version: Literal["2015-06-01-preview"] = kwargs.pop(
-        "api_version", _params.pop("api-version", "2015-06-01-preview")
-    )
+    api_version: str = kwargs.pop("api_version", _params.pop("api-version", "2015-06-01-preview"))
     accept = _headers.pop("Accept", "application/json, text/json")
 
     # Construct URL
@@ -149,9 +142,7 @@ class UsageAggregatesOperations:
         _headers = kwargs.pop("headers", {}) or {}
         _params = case_insensitive_dict(kwargs.pop("params", {}) or {})
 
-        api_version: Literal["2015-06-01-preview"] = kwargs.pop(
-            "api_version", _params.pop("api-version", self._config.api_version)
-        )
+        api_version: str = kwargs.pop("api_version", _params.pop("api-version", self._config.api_version))
         cls: ClsType[_models.UsageAggregationListResult] = kwargs.pop("cls", None)
 
         error_map = {
@@ -208,14 +199,15 @@ class UsageAggregatesOperations:
         def get_next(next_link=None):
             request = prepare_request(next_link)
 
+            _stream = False
             pipeline_response: PipelineResponse = self._client._pipeline.run(  # pylint: disable=protected-access
-                request, stream=False, **kwargs
+                request, stream=_stream, **kwargs
             )
             response = pipeline_response.http_response
 
-            if response.status_code not in [200]:
+            if response.status_code not in [200, 202]:
                 map_error(status_code=response.status_code, response=response, error_map=error_map)
-                error = self._deserialize.failsafe_deserialize(_models.ErrorResponse, pipeline_response)
+                error = self._deserialize.failsafe_deserialize(_models.ErrorObjectResponse, pipeline_response)
                 raise HttpResponseError(response=response, model=error, error_format=ARMErrorFormat)
 
             return pipeline_response
