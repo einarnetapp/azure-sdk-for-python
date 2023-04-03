3979da282efc85590fc34e47a7604fe673d18518
SDKAuto <sdkautomation@microsoft.com>
2023-04-03 18:39:07 +0000

    CodeGen from PR 23408 in Azure/azure-rest-api-specs Merge e59d3a0342d18ded97d3c2be4ab20e13d2ba65d3 into 886267cb0c43fc33535d5d2b44479cf6a0b641d3

--- sdk/dashboard/azure-mgmt-dashboard/azure/mgmt/dashboard/_dashboard_management_client.py
+++ sdk/dashboard/azure-mgmt-dashboard/azure/mgmt/dashboard/_dashboard_management_client.py
@@ -62,7 +62,7 @@ class DashboardManagementClient:  # pylint: disable=client-accepts-api-version-k
         self._config = DashboardManagementClientConfiguration(
             credential=credential, subscription_id=subscription_id, **kwargs
         )
-        self._client = ARMPipelineClient(base_url=base_url, config=self._config, **kwargs)
+        self._client: ARMPipelineClient = ARMPipelineClient(base_url=base_url, config=self._config, **kwargs)
 
         client_models = {k: v for k, v in _models.__dict__.items() if isinstance(v, type)}
         self._serialize = Serializer(client_models)
@@ -106,5 +106,5 @@ class DashboardManagementClient:  # pylint: disable=client-accepts-api-version-k
         self._client.__enter__()
         return self
 
-    def __exit__(self, *exc_details) -> None:
+    def __exit__(self, *exc_details: Any) -> None:
         self._client.__exit__(*exc_details)
